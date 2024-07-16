d1b8808df269f188eaa26984200e4d0bda4aee24
Tuan Hoang <tonytuan75470@gmail.com>
2024-07-16 11:36:28 +0700

    [07-16-24] update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Travel",
-  description: "Travel test",
+  description: "Travel",
 };
 
 export default function RootLayout({
